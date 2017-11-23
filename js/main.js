4dc84aad86d5ac66a75d61f0388b758bfd38fb97
Himanshu <ptlhimanshu827@gmail.com>
2017-11-23 07:49:02 +0530

    Parallax Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+		// $(window).on('load', function() {
+ //            $('.preloader').delay(350).fadeOut('slow');
+ //            $('.preloader-wrapper').delay(350).fadeOut();
+ //        });
+ 	$('.parallax').parallax();
 	$(".button-collapse").sideNav();
+
 })
