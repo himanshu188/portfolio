885635a81f6ea2587326dbd50ff15745f8a287aa
Himanshu <ptlhimanshu827@gmail.com>
2018-03-19 21:18:45 +0530

    Skills / Certi Added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
  //            $('.preloader').delay(350).fadeOut('slow');
  //            $('.preloader-wrapper').delay(350).fadeOut();
  //        });
+ 	$('.modal').modal();
  	$('.parallax').parallax();
 	$(".button-collapse").sideNav();
-
+	$('.materialboxed').materialbox();
 })
