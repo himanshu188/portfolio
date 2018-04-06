795e910c219f7ab843d70453d777062f13e2b829
Himanshu <ptlhimanshu827@gmail.com>
2018-04-06 11:31:12 +0530

    Minor Bug Fixing

--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
  //            $('.preloader-wrapper').delay(350).fadeOut();
  //        });
  	$('.modal').modal();
+ 	// $('.sidenav').sidenav();
  	$('.parallax').parallax();
 	$(".button-collapse").sideNav();
+	// $(".sidenav-trigger").sideNav();
 	$('.materialboxed').materialbox();
+
+
 })
